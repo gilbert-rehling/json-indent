bcef88869a346c198c7c291f2e6bc897a69bc7f2
Gilbert Rehling <gilbert.rehling@gmail.com>
2020-08-13 14:47:38 +1000

    Update json-indent.js

--- json-indent.js
+++ json-indent.js
@@ -17,7 +17,7 @@
  *
  * @type {string}
  */
-let str = '{"_id":"r32435451dff323r323f3f","blog":{"dogs":{"small":"daschund","medium":"cattle dog","big":"great dane"}},"cats":{"furry":"fluffy","scardy":"cat"},"tv":[{"leo":"lion"},{"sylvester":"disney"},"felix","alley"],"count":7}';
+let str = '{"_id":ObjectId("r32435451dff323r323f3f"),"blog":{"dogs":{"small":"daschund","medium":"cattle dog","big":"great dane"}},"cats":{"furry":"fluffy","scardy":"cat","tv":[{"leo":"lion","clarence":"lion"},{"sylvester":"disney"},"felix","alley"]},"count":7}';
 
 /**
  * Textarea first (\n & \t)
@@ -192,3 +192,105 @@ for (x in str) {
 // console.log(divArray);
 // display
 document.getElementById('div-array').innerHTML = divArray;
+
+/**
+ * For div display with color highlights (<br> and TAB (&nbsp;&nbsp;&nbsp;&nbsp;)
+ * Leave as JSON
+ */
+openB = 0;
+index = 0;
+let divAreaH = '';
+for (x in str) {
+    let plus = (parseInt(x) + 1);
+
+    if (str[x] === '{' || str[x] === '[') {
+        openB += 1;
+        divAreaH += '<span style="color: green">' + str[x] + '</span>' + '<br>';
+        for (i = 0; i < openB; i += 1) {
+            divAreaH += tab;
+        }
+    }
+    else if (str[x] === '}' || str[x] === ']') {
+        divAreaH += '<br>';
+
+        if (openB > 0) {
+            openB -= 1;
+        }
+        for (i = 0; i < openB; i += 1) {
+            divAreaH += tab;
+        }
+        divAreaH += '<span style="color: green">' + str[x] + '</span>';
+    }
+    else if (str[x] === '(' || str[x] === ')') {
+        divAreaH += '<span style="color: blue">' + str[x] + '</span>';
+    }
+    else if (str[x] === ',') {
+        divAreaH += '<span style="color: green">' + str[x] + '</span>' + '<br>';
+
+        for (i = 0; i < openB; i += 1) {
+            divAreaH += tab;
+        }
+    }
+    else if (str[x] === ':') {
+        divAreaH += '<span style="color: green">' + str[x] + '</span>';
+    }
+    else if (str[x] === '"') {
+        divAreaH += '<span style="color: #800000">' + str[x] + '</span>';
+    }
+    else {
+        divAreaH += '<span style="color: red">' + str[x] + '</span>';
+    }
+}
+//console.log(divArea);
+// display
+document.getElementById('div-area-highlight').innerHTML = divAreaH;
+
+/**
+ * For DIV display
+ * To array format
+ */
+openB = 0;
+index = 0;
+let divArrayH = '';
+for (x in str) {
+    if (str[x] === '{' || str[x] === '[') {
+        openB += 1;
+        divArrayH += '<span style="color: blue">array(</span><br>';
+        for (i = 0; i < openB; i += 1) {
+            divArrayH += tab;
+        }
+    }
+    else if (str[x] === '}' || str[x] === ']') {
+        divArrayH += '<br>';
+
+        if (openB > 0) {
+            openB -= 1;
+        }
+        for (i = 0; i < openB; i += 1) {
+            divArrayH += tab;
+        }
+        divArrayH += '<span style="color: blue">)</span>';
+    }
+    else if (str[x] === ',') {
+        divArrayH += '<span style="color: green">' + str[x] + '</span>' + '<br>';
+
+        for (i = 0; i < openB; i += 1) {
+            divArrayH += tab;
+        }
+    }
+    else if (str[x] === '(' || str[x] === ')') {
+        divArrayH += '<span style="color: green">' + str[x] + '</span>';
+    }
+    else if (str[x] === ":") {
+        divArrayH += ' <span style="color: green">=></span> ';
+    }
+    else if (str[x] === '"') {
+        divArrayH += '<span style="color: #800000">' + str[x] + '</span>';
+    }
+    else {
+        divArrayH += '<span style="color: red">' + str[x] + '</span>';
+    }
+}
+// console.log(divArrayH);
+// display
+document.getElementById('div-array-highlight').innerHTML = divArrayH;
